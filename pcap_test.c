79da61519cd1002b31e295b711f9505258db88d1
GrayField <mdc4865@gmail.com>
2017-10-30 01:57:39 +0900

    parser

--- pcap_test.c
+++ pcap_test.c
@@ -6,75 +6,208 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <string.h>
 
-/* Callback function invoked by libpcap for every incoming packet */
-// void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data)
-// {
-//     struct ether_header *eth_header;
-//     eth_header = (struct ether_header *) pkt_data;
+#define	IEEE80211_ADDR_LEN	6
+#define	IEEE80211_FC0_SUBTYPE_QOS	0x80
+#define SIZE_OF_IEEE_802_11_QOS_FRAME 28
+#define ETHTYPE_IP 0x08
+#define PROTO_TCP 0x06
 
-//     if (ntohs(eth_header->ether_type) == ETHERTYPE_IP) {
-//         printf("IP\n");
-//     } else  if (ntohs(eth_header->ether_type) == ETHERTYPE_ARP) {
-//         printf("ARP\n");
-//     } else  if (ntohs(eth_header->ether_type) == ETHERTYPE_REVARP) {
-//         printf("Reverse ARP\n");
-//     }    
-// }
+typedef struct radiotap_header{ // RadioTap is the standard for 802.11 reception/transmission/injection
+    uint8_t it_rev; // Revision: Version of RadioTap
+    uint8_t it_pad; // Padding: 0 - Aligns the fields onto natural word boundaries
+    uint16_t it_len;
+}radiotap_header;
+
+typedef struct ieee80211_frame {
+	u_int8_t	i_fc[2];
+	u_int8_t	i_dur[2];
+	u_int8_t	i_addr1[IEEE80211_ADDR_LEN];
+	u_int8_t	i_addr2[IEEE80211_ADDR_LEN];
+	u_int8_t	i_addr3[IEEE80211_ADDR_LEN];
+	u_int8_t	i_seq[2];
+} ieee80211_frame;
+
+typedef struct ieee80211_qosframe {
+	u_int8_t	i_fc[2];
+	u_int8_t	i_dur[2];
+	u_int8_t	i_addr1[IEEE80211_ADDR_LEN];
+	u_int8_t	i_addr2[IEEE80211_ADDR_LEN];
+	u_int8_t	i_addr3[IEEE80211_ADDR_LEN];
+	u_int8_t	i_seq[2];
+	u_int8_t	i_qos[2];
+} ieee80211_qosframe;
+
+typedef struct llc_frame {
+    u_int8_t    i_dsap;
+    u_int8_t    i_ssap;
+    u_int8_t    i_ctrl;
+    u_int8_t    i_org[3];
+    u_int16_t   i_ethtype;
+} llc_frame;
+
+typedef struct ip_frame {
+    u_int8_t    ver;
+    u_int8_t    tos;
+    u_int16_t   tot_len;
+    u_int16_t   id;
+    u_int16_t   frag_off;
+    u_int8_t    ttl;
+    u_int8_t    protocol;
+    u_int16_t   check;
+    u_int32_t   saddr;
+    u_int32_t   daddr;
+} ip_frame;
+
+typedef struct tcp_hdr {
+    u_int16_t sport;
+    u_int16_t dport;
+    u_int32_t seqnu;
+    u_int32_t ack_seq;
+    //u_int16_t len_flags;
+    u_int16_t res1:4,
+              doff:4,
+              fin:1,
+              syn:1,
+              rst:1,
+              psh:1,
+              ack:1,
+              urg:1,
+              ece:1,
+              cwr:1;
+    u_int16_t window;
+    u_int16_t checksum;
+    u_int16_t urg_ptr;
+} tcp_hdr;
+
+void parse_data_get(const u_char *data)
+{
+    const u_char * start_ptr = NULL;
+    const u_char * end_ptr = NULL;
+    const u_char get_url[256] = {0};
+    const u_char host[256] = {0};
+    const u_char cookie[512] = {0};
+
+    start_ptr = strstr(data, "GET");
+    if(start_ptr != NULL) {
+        end_ptr = strstr(start_ptr, "\r\n");
+        memcpy(get_url, start_ptr, end_ptr-start_ptr);
+        printf("%s\n", get_url);
+    }
+
+    start_ptr = strstr(data, "Host: ");
+    if(start_ptr != NULL) {
+        end_ptr = strstr(start_ptr, "\r\n");
+        memcpy(host, start_ptr, end_ptr-start_ptr);
+        printf("%s\n", host);
+    }
+
+    start_ptr = strstr(data, "Cookie: ");
+    if(start_ptr != NULL) {
+        end_ptr = strstr(start_ptr, "\r\n");
+        if(end_ptr != NULL)
+            memcpy(cookie, start_ptr, end_ptr-start_ptr);
+        else {
+            memcpy(cookie, start_ptr, strlen(start_ptr));
+        }
+        printf("%s\n", cookie);
+    }
+    puts("");
+
+}
 
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
-	// This struct is the RadioTap header: https://radiotap.org
-	struct radiotap_header{ // RadioTap is the standard for 802.11 reception/transmission/injection
-		uint8_t it_rev; // Revision: Version of RadioTap
-		uint8_t it_pad; // Padding: 0 - Aligns the fields onto natural word boundaries
-		uint16_t it_len;// Length: 26 - entire length of RadioTap header
-	};
-	// These are placeholders for offset values:
-	const u_char *bssid; // a place to put our BSSID \ these are bytes
-	const u_char *essid; // a place to put our ESSID / from the packet
-	const u_char *essidLen;
-	const u_char *channel; // the frequency (in Mhz) of the AP Radio
-	const u_char *rssi; // received signal strength
 
-	int offset = 0;
-	struct radiotap_header *rtaphdr;
-	rtaphdr = (struct radiotap_header *) packet;
-	offset = rtaphdr->it_len; // 26 bytes on my machine
-	//if(packet[offset]==0x80){ // 0x80 is 128 in dec. It is a Beacon MGMT frame // REMOVED for BPF syntax
-	bssid = packet + 42; // store the BSSID/AP MAC addr, 36 byte offset is transmitter address
-	essid = packet + 64; // store the ESSID/Router name too
-	essidLen = packet + 63; // store the ESSID length // this can be used to avoid looping bytes until >0x1 as below
-	rssi = packet + 22; // this is hex and this value is subtracted from 256 to get -X dbm.
-	signed int rssiDbm = rssi[0] - 256;
-	channel = packet + 18; // channel in little endian format (2 bytes)
-	int channelFreq = channel[1] * 256 + channel[0]; // a little bit of math, remember little endian
-	// 87 byte offset contains the "channel number" as per 802.11, e.g. 2412 = "channel 11"
-	char *ssid = malloc(63); // 63 byte limit
-	unsigned int i = 0; // used in loop below:
-	while(essid[i] > 0x1){ // uncomment these to see each byte individually:
-		//printf ("hex byte: %x\n",essid[i]); // view byte
-		//printf ("hex char: %c\n",essid[i]); // view ASCII
-		ssid[i] = essid[i]; // store the ESSID bytes in *ssid
-		i++; // POSTFIX
-	}
-	ssid[i] = '\0'; // terminate the string
-	fprintf(stdout,"RSSI: %d dBm\n",rssiDbm);
-	fprintf(stdout,"AP Frequency: %iMhz\n",channelFreq);
-	fprintf(stdout,"ESSID length: %i bytes.\n",essidLen[0]);
-	fprintf(stdout,"ESSID string: %s\n", ssid); // print the stored ESSID bytes
-    fprintf(stdout,"BSSID string: %02X:%02X:%02X:%02X:%02X:%02X\n",bssid[0],bssid[1],bssid[2],bssid[3],bssid[4],bssid[5]);
-    fprintf(stdout,"All packet  : %s\n", packet);
-	//} // REMOVED for BPF syntax
-	
-	// Let's write the beacon to a file:
-	pcap_dumper_t *outputFile;
-	pcap_t *fileHandle;
-	char *outputFileName = "output.cap";
-	fileHandle = pcap_open_dead(DLT_IEEE802_11_RADIO, BUFSIZ);
-	outputFile = pcap_dump_open(fileHandle,outputFileName);
-	pcap_dump((u_char *) outputFile,header, packet);
-    pcap_close(fileHandle);
+    int size_80211hdr = 0, offset_ip = 0, offset_proto = 0, length=0;
+    radiotap_header *rtaphdr;
+    ieee80211_frame *wfhdr;
+    ieee80211_qosframe *wqfhdr;
+    llc_frame *llchdr;
+    ip_frame *iphdr;
+    tcp_hdr *tcphdr;
+
+    u_int8_t pkt[2048] = {0};
+
+    length = header->caplen;
+
+	rtaphdr = (radiotap_header *) packet;
+
+    // skip to next header
+    packet += rtaphdr->it_len;
+    length -= rtaphdr->it_len;
+
+    wfhdr = (ieee80211_frame * ) packet;
+    size_80211hdr = sizeof(ieee80211_frame);
+
+    if( 0x88 != wfhdr->i_fc[0]) {
+        return;
+    }
+
+    wqfhdr = (ieee80211_qosframe *) packet;
+    size_80211hdr = sizeof(ieee80211_qosframe);
+
+    // printf("FC: %x\n", wfhdr->i_fc[0]);
+    // printf("Flag: %x\n", wqfhdr->i_fc[1]);
+    // printf("Duration: %d microsec\n", ((int)wqfhdr->i_dur[0])*128 + (int)wqfhdr->i_dur[1]);
+    // printf("%2x:%2x:%2x:%2x:%2x:%2x\n", wqfhdr->i_addr1[0], wqfhdr->i_addr1[1], wqfhdr->i_addr1[2], wqfhdr->i_addr1[3], wqfhdr->i_addr1[4],wqfhdr->i_addr1[5],wqfhdr->i_addr1[6]);  
     
+    // skip to next header
+    packet += size_80211hdr;
+    length -= size_80211hdr;
+
+    llchdr = (llc_frame*) packet;
+
+    // skip to next header
+    packet += sizeof(struct llc_frame);
+
+    // i_ethtype is ip
+    if((short)ETHTYPE_IP == llchdr->i_ethtype)
+    {
+        offset_ip = size_80211hdr + sizeof(llc_frame);
+        offset_proto = offset_ip + sizeof(ip_frame);
+
+        iphdr = (ip_frame*)packet;
+
+        // skip to next header
+        packet += sizeof(ip_frame);
+        length -= sizeof(ip_frame);
+
+        // protocol is tcp
+        if((short)PROTO_TCP == iphdr->protocol)
+        {
+            tcphdr = (tcp_hdr*)packet;
+
+            // http
+            if (80 == ntohs(tcphdr->dport))
+            {
+                // printf("HTTP\n");
+                u_int32_t hdr_size = tcphdr->doff*4;
+                u_int8_t *p_http = packet + hdr_size;
+                u_int32_t l_http = length - hdr_size-6;
+                memcpy(pkt, p_http, l_http);
+
+                // find get
+                if( p_http[0] == 0x47 &&
+                    p_http[1] == 0x45 &&
+                    p_http[2] == 0x54)
+                {
+                    // printf("%s\n", p_http);
+                    parse_data_get(pkt);
+                }
+
+                //find post
+                if( p_http[0] == 0x50 &&
+                    p_http[1] == 0x4F &&
+                    p_http[2] == 0x53 &&
+                    p_http[4] == 0x54)
+                {
+                    // printf("%s\n", p_http);
+                    // parse_data(pkt);
+                }
+            }
+        }
+    }
 
 	return;
 }
@@ -86,6 +219,7 @@ int main(int argc, char **argv)
         char *dev;
         char *net;
         char *mask;
+        int result;
 
         int ret = 0, i = 0, inum = 0, check=0;
         pcap_if_t *alldevs;
@@ -164,7 +298,7 @@ int main(int argc, char **argv)
         {
             if(check==1)
             {
-                printf("Monitor mode could be set\n");
+                // printf("Monitor mode could be set\n");
                 if(pcap_set_rfmon(handle, 1))
                 {
                     printf("Can not set monitor mode\n");
@@ -198,10 +332,13 @@ int main(int argc, char **argv)
 
         printf("Start\n");
 
-        pcap_loop(handle, 0, packet_handler, NULL);
-
-        pcap_close(handle);
         pcap_freealldevs(alldevs);
+
+        result = pcap_loop(handle, 0, packet_handler, NULL);
+        
+        printf("%d: Pcap Loop is terminated\n", result);
+        pcap_close(handle);
+        
 
         return 0;
 
